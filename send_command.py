f2b783c173c89cbcb05d6127cb4c4cac25f7cc1c
ronak-sirwani <42727467+ronak-sirwani@users.noreply.github.com>
2022-03-31 13:28:58 +0530

    Add files via upload

--- send_command.py
+++ send_command.py
@@ -1,20 +1,23 @@
 import paramiko
+import time
 
 nbytes = 4096
-hostname = ['172.16.13.164', '172.16.12.176']
+hostname = ['172.16.13.164']
 port = 22
 username = "meditab"
 password = "99$inmedi"
-command = 'notify-send "HELLO"'
+command = ['notify-send "HELLO"', 'notify-send "Your system is compromised!!"']
 
-for host in range(len(hostname)):
-    client = paramiko.Transport((hostname[host], port))
+def send_commands(hostname, port, username, password, command, nbytes):
+    client = paramiko.Transport((hostname, port))
     client.connect(username=username, password=password)
 
     stdout_data = []
     stderr_data = []
     session = client.open_channel(kind='session')
+
     session.exec_command(command)
+
     while True:
         if session.recv_ready():
             stdout_data.append(session.recv(nbytes))
@@ -23,9 +26,13 @@ for host in range(len(hostname)):
         if session.exit_status_ready():
             break
 
-    print ("exit status: ", session.recv_exit_status())
-    print (stdout_data)
-    print (stderr_data)
+    print("exit status: ", session.recv_exit_status())
+    print(stdout_data)
+    print(stderr_data)
 
     session.close()
     client.close()
+
+for host in range(len(hostname)):
+    for i in command:
+        send_commands(hostname[host], port, username, password, i, nbytes)
